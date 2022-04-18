2fd6bfbff47f787ecd27b20046ccf6f4e084316c
Piero Caceres <pierocaceres92@yahoo.com>
2022-04-18 09:49:03 -0400

    Added AuthController: Created routes for Register and Login

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,13 @@
 const Router = require('express').Router()
 const controller = require('../controller/AllController')
+const authController = require('../controller/AuthController')
 const middleware = require('../middleware')
 
-//##################  BLOG ROUTE ######################//
+//##################  AUTHENTICATION ROUTE  ######################//
+Router.post('/register', authController.Register)
+Router.post('/login', authController.Login)
+
+//##################  BLOG ROUTE  ######################//
 //Calls Blog Orders by Date Created. Homepage
 Router.get('/blog/all', controller.GetAllBlogs)
 //Blog Endpoint, Pulls comments, Where user reads article
@@ -14,18 +19,19 @@ Router.put('/blog/update/:blog_id',controller.EditBlog)
 //Delete Blog Post
 Router.delete('/blog/delete/:blog_id', controller.DeleteBlog)
 
-//##################  COMMENT ROUTE ######################//
+//##################  COMMENT ROUTE  ######################//
 //Create New Comment
 Router.post('/comment/new/:author_id/:blog_id', controller.PostComment)
 //Edit Comment
 Router.put('/comment/edit/:comment_id', controller.EditComment)
 //Like Comment
 Router.put('/comment/like/:comment_id',  controller.LikeComment)
+//Dislike Comment
 Router.put('/comment/dislike/:comment_id',  controller.DislikeComment)
 //Delete Comment
 Router.delete('/comment/delete/:comment_id', controller.DeleteComment)
 
-// //##################  REPLY ROUTE ######################//
+// //##################  REPLY ROUTE  ######################//
 // Router.post()
 // Router.put()
 // Router.delete()
